44e51adeb64ba743eeded90f6abb5f0bf730cc40
Jabari Phillip <jabari.phillip@my.uwi.edu>
2026-01-30 14:53:42 -0400

    changes to lab 1 and 2

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ with open('./data.json') as f:
 @app.get('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.get('/shuttles')
+async def get_shuttles_overview():
+    shuttles_list = []
+
+    for shuttle in data:
+        curr_data = {
+            "name": shuttle['name'],
+            "reg_no": shuttle['vehicle_details']['reg_no'],
+            "active": shuttle['active'],
+        }
+        shuttles_list.append(curr_data)
+    return shuttles_list
