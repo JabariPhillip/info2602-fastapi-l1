b0902822f5fdc05351c7f4bf9b7a53bba6f88f4a
Jabari Phillip <jabari.phillip@my.uwi.edu>
2026-02-02 20:38:41 -0400

    Lab 1 exercises

--- main.py
+++ main.py
@@ -12,3 +12,50 @@ with open('./data.json') as f:
 @app.get('/')
 def hello_world():
     return 'Hello, World!'
+      
+@app.get('/students')
+async def get_students(pref=None):
+    if pref:
+        filtered_students = []
+        for student in data:
+            if student['pref'] == pref:
+                filtered_students.append(student)
+        return filtered_students
+    return data
+
+@app.get('/stats')
+async def get_stats():
+    stats = {}
+
+    for student in data:
+        pref = student["pref"]
+        if pref in stats:
+            stats[pref] += 1
+        else:
+            stats[pref] = 1
+
+        programme = student["programme"]
+        if programme in stats:
+            stats[programme] += 1
+        else:
+            stats[programme] = 1
+
+    return stats
+
+@app.get('/add/{a}/{b}')
+async def add(a: int, b: int):
+    return {"result": a + b}
+
+@app.get('/subtract/{a}/{b}')
+async def subtract(a: int, b: int):
+    return {"result": a - b}
+
+@app.get('/multiply/{a}/{b}')
+async def multiply(a: int, b: int):
+    return {"result": a * b}
+
+@app.get('/divide/{a}/{b}')
+async def divide(a: int, b: int):
+    if b > 0:
+        return {"result": a / b}
+    return{"error": "Can not divide by 0"}
